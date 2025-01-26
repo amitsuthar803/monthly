1cf613e05129c3476a2ad85aa1709f6d7b5d7bc7
amitsuthar803 <amitsuthar803@gmail.com>
2025-01-26 22:56:27 +0530

    update

---
+++ src/config/env.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyBQVbwFhCrboedDj86u1XR8I13hZni4E5Q',
+  authDomain: 'monthly-a36ce.firebaseapp.com',
+  projectId: 'monthly-a36ce',
+  storageBucket: 'monthly-a36ce.firebasestorage.app',
+  messagingSenderId: '146369426758',
+  appId: '1:146369426758:web:400efe38ca9c394795a147',
+  measurementId: 'G-GDB17RBZDZ',
+};
